0f423c2f595e5388acee0f78c3df9b2dbd6ca9d5
l0la960 <oyinlolaolaniyan47@gmail.com>
2026-09-15 11:15:49 +0100

    Form and Email Validation for creating a task

--- src/App.jsx
+++ src/App.jsx
@@ -1,142 +1,174 @@
-import React, { useState } from 'react'
-import Header from "./Header"
-import MainContent from "./MainContent"
-import CreateTaskForm from "./CreateTaskForm"
-import UpdateForm from './UpdateEmailAlert'
-
-
+import React, { useState } from "react";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import CreateTaskForm from "./CreateTaskForm";
+import UpdateForm from "./UpdateEmailForm";
 
 function App() {
+  const [isEmailAlertEnabled, setEmailAlertEnabled] = useState(false);
 
   // todo categories states
-  const [todoList, setTodolist] = useState([])
-  const [doingList, setDoingtList] = useState([])
-  const [doneList, setDoneList] = useState([])
+  const [todoList, setTodolist] = useState([]);
+  const [doingList, setDoingtList] = useState([]);
+  const [doneList, setDoneList] = useState([]);
 
-
-
-// form states open and close
-  const [isFormOpened, setFormOpened] = useState(false)
+  // form states open and close
+  const [isFormOpened, setFormOpened] = useState(false);
 
   const openForm = () => {
-    setFormOpened(true)
-    console.log(open)
-  }
+    setFormOpened(true);
+    console.log(open);
+  };
 
   const closeForm = () => {
-    setFormOpened(false)
-    console.log(close)
-  }
+    setFormOpened(false);
+    console.log(close);
+  };
 
-
-    const [isUpdateFormOpened, setUpdateFormOpened] = useState(false)
+  const [isUpdateFormOpened, setUpdateFormOpened] = useState(false);
 
   const openUpdateForm = () => {
-    setUpdateFormOpened(true)
-    console.log(open)
-  }
+    setUpdateFormOpened(true);
+    console.log(open);
+  };
 
   const closeUpdateForm = () => {
-    setUpdateFormOpened(false)
-    console.log(close)
-  }
-   
+    setUpdateFormOpened(false);
+    console.log(close);
+  };
 
-  
+  //add to to list function and form validation on the button
 
-
-
-  //add to to list function 
   const addTodolist = (taskItem) => {
-    if (taskItem.title.trim() === '' || taskItem.dueDate.trim() === '') {
-    document.getElementById('task-title').style.outline = '5px solid oklch(70.9% 0.00008 271.152 / 0.722)'
-    document.getElementById('due-date').style.outline = '5px solid oklch(70.9% 0.00008 271.152 / 0.722)'
-    }
-
-    else {
-    setTodolist(t => [...t, taskItem])
-    closeForm()
-     } 
+    function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email,
+    );
   }
+    let title = taskItem.title.trim();
+    let dueDate = taskItem.dueDate.trim();
+    let email = taskItem.emailAddress.trim();
+    let alertDate = taskItem.alertDate.trim();
 
+    if (!isEmailAlertEnabled && (title === "" || dueDate === "")) {
+      document.getElementById("task-title").style.outline =
+        "5px solid oklch(70.9% 0.00008 271.152 / 0.722)";
+      document.getElementById("due-date").style.outline =
+        "5px solid oklch(70.9% 0.00008 271.152 / 0.722)";
+    } else if (!isEmailAlertEnabled && (title !== "" || dueDate !== "")) {
+      setTodolist((t) => [...t, taskItem]);
+      closeForm();
+    } else if (
+      isEmailAlertEnabled &&
+      (email === "" || alertDate === "" || title === "" || dueDate === "")
+    ) {
+      document.getElementById("email-address").style.outline =
+        "5px solid oklch(70.9% 0.00008 271.152 / 0.722)";
+      document.getElementById("alert-date").style.outline =
+        "5px solid oklch(70.9% 0.00008 271.152 / 0.722)";
+      document.getElementById("task-title").style.outline =
+        "5px solid oklch(70.9% 0.00008 271.152 / 0.722)";
+      document.getElementById("due-date").style.outline =
+        "5px solid oklch(70.9% 0.00008 271.152 / 0.722)";
+    } 
+    else if (isEmailAlertEnabled && !isEmail(email)) {
+    console.log('invalid email')
+    }
+    else {
+      setTodolist((t) => [...t, taskItem]);
+      closeForm();
+    }
+  };
 
-  
   // move functions
 
   const moveTaskDone = (index) => {
-    deleteTodo(index)
+    deleteTodo(index);
     const doneTodoItem = {
       name: todoList[index].title,
       description: todoList[index].description,
       dueDate: todoList[index].dueDate,
-      dueTime: todoList[index].dueTime, 
-    } 
-    setDoneList(d => [...d, doneTodoItem])
-
-  }
-
+      dueTime: todoList[index].dueTime,
+    };
+    setDoneList((d) => [...d, doneTodoItem]);
+  };
 
   const moveTaskDoing = (index) => {
-     deleteTodo(index)
+    deleteTodo(index);
     const doingTodoItem = {
       name: todoList[index].title,
       description: todoList[index].description,
       dueDate: todoList[index].dueDate,
-      dueTime: todoList[index].dueTime, 
-      
-    }
-    setDoingtList(d => [...d, doingTodoItem])
-
-  } 
-
-
+      dueTime: todoList[index].dueTime,
+    };
+    setDoingtList((d) => [...d, doingTodoItem]);
+  };
 
   // Quantity decrement states for doing done todo
 
-  const [todoQuantity, setTodoQuantity] = useState(todoList.length)
-  const [doneQuantity, setDoneQuantity] = useState(doneList.length)
-  const [doingQuantity, setDoingQuantity] = useState(doingList.length)
-
+  const [todoQuantity, setTodoQuantity] = useState(todoList.length);
+  const [doneQuantity, setDoneQuantity] = useState(doneList.length);
+  const [doingQuantity, setDoingQuantity] = useState(doingList.length);
 
   const decrementTodoQuantity = () => {
-    setTodoQuantity(todoList.length - 1)
-  }
+    setTodoQuantity(todoList.length - 1);
+  };
 
   const decrementDoneQuantity = () => {
-    setDoneQuantity(doneList.length - 1)
+    setDoneQuantity(doneList.length - 1);
   };
 
-
-   const decrementDoingQuantity = () => {
-    setDoingQuantity(doingList.length - 1)
+  const decrementDoingQuantity = () => {
+    setDoingQuantity(doingList.length - 1);
   };
 
+  // delete functions for each category
 
-   // delete functions for each category
-
-   const deleteTodo = (index) => {
-    setTodolist(todoList.filter((_, i) => i !== index))
-    decrementTodoQuantity()
-  }
+  const deleteTodo = (index) => {
+    setTodolist(todoList.filter((_, i) => i !== index));
+    decrementTodoQuantity();
+  };
 
   const deleteDoneTodo = (index) => {
-    setDoneList(doneList.filter((_, i) => i !== index))
-    decrementDoneQuantity()
-  }
+    setDoneList(doneList.filter((_, i) => i !== index));
+    decrementDoneQuantity();
+  };
 
   const deleteDoingTodo = (index) => {
-    setDoingtList(doingList.filter((_, i) => i !== index))
-    decrementDoingQuantity()
-  }
-  
+    setDoingtList(doingList.filter((_, i) => i !== index));
+    decrementDoingQuantity();
+  };
+
   return (
     <>
       <Header onClick={() => openForm()} />
-      <MainContent tasks={todoList} doneTasks={doneList} doingTasks={doingList} deleteDoingTodo={deleteDoingTodo} deleteItem ={deleteTodo} deleteDoneTodo={deleteDoneTodo} moveTaskDone={moveTaskDone} moveTaskDoing={moveTaskDoing} updateForm={()=> openUpdateForm()}/>
-      {isFormOpened && <CreateTaskForm onClick={() => closeForm()} addTasks={addTodolist}/>}
-      {isUpdateFormOpened && <UpdateForm closeUpdateForm={() => closeUpdateForm()} taskTitle={todoList.title}/> }
+      <MainContent
+        tasks={todoList}
+        doneTasks={doneList}
+        doingTasks={doingList}
+        deleteDoingTodo={deleteDoingTodo}
+        deleteItem={deleteTodo}
+        deleteDoneTodo={deleteDoneTodo}
+        moveTaskDone={moveTaskDone}
+        moveTaskDoing={moveTaskDoing}
+        updateForm={() => openUpdateForm()}
+      />
+      {isFormOpened && (
+        <CreateTaskForm
+          onClick={() => closeForm()}
+          addTasks={addTodolist}
+          isEmailAlertEnabled={isEmailAlertEnabled}
+          setEmailAlertEnabled={setEmailAlertEnabled}
+        />
+      )}
+      {isUpdateFormOpened && (
+        <UpdateForm
+          closeUpdateForm={() => closeUpdateForm()}
+          taskTitle={todoList.title}
+        />
+      )}
     </>
   );
 }
 
-export default App
+export default App;
--- src/CreateTaskForm.jsx
+++ src/CreateTaskForm.jsx
@@ -1,126 +1,215 @@
-import React, {useState} from 'react';
-import { useEffect } from "react"
-import crossIcon from './assets/cross-small.png';
-import Switch from './components/SwitchButton';
-import Button from './components/Button';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import crossIcon from "./assets/cross-small.png";
+import Switch from "./components/SwitchButton";
+import Button from "./components/Button";
+
+export default function CreateTaskForm({ onClick, addTasks, isEmailAlertEnabled, setEmailAlertEnabled }) {
+  
+
+  // Const [isEmailAlertEnabled, setEmailAlertEnabled] = useState(false);
+  
+  const enableEmailAlert = () => {
+    setEmailAlertEnabled(!isEmailAlertEnabled);
+  };
 
 
 
-export default function CreateTaskForm ({onClick, addTasks}) {
+  const [taskTitle, setTaskTitle] = useState("");
+  const taskTitleChange = (e) => {
+    setTaskTitle(e.target.value);
+  };
 
-const [isEmailAlertEnabled,setEmailAlertEnabled] = useState(false)
-const enableEmailAlert = () => {
-setEmailAlertEnabled(!isEmailAlertEnabled)
+  const [description, setDescription] = useState("");
+  const descriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const [dueDate, setDueDate] = useState("");
+  const dueDateChange = (e) => {
+    setDueDate(e.target.value);
+  };
+
+  const [dueTime, setDueTime] = useState("");
+  const dueTimeChange = (e) => {
+    setDueTime(e.target.value);
+  };
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const emailAddressChange = (e) => {
+    setEmailAddress(e.target.value);
+  };
+
+  const [alertDate, setAlertDate] = useState("");
+  const alertDateChange = (e) => {
+    setAlertDate(e.target.value);
+  };
+
+  const [alertTime, setAlertTime] = useState("");
+  const alertTimeChange = (e) => {
+    setAlertTime(e.target.value);
+  };
+
+  useEffect(() => {
+     if (isEmailAlertEnabled) {
+    document.getElementById("form-container-id").style.overflowY = "scroll";
+    document.getElementById("form-container-id").style.scrollbarColor = "";
+    document.getElementById("form-container-id").style.scrollbarWidth = "thin";
+  }
+  })
+
+  return (
+    <>
+      <div className="form-background">
+        <div className="form-container" id="form-container-id">
+          <div className="form-exit-container">
+            <button className="form-exit-btn" onClick={onClick}>
+              <img src={crossIcon}></img>
+            </button>
+          </div>
+          <form>
+            <div className="form-container-heading">
+              <h2>Create New Task</h2>
+              <p>Add a new task with a due date and optional email alerts.</p>
+            </div>
+
+            <FormInputContainer
+              id="task-title"
+              autofocus="autofocus"
+              label="Task Title*"
+              placeholder="Enter task title"
+              type="text"
+              value={taskTitle}
+              onChange={(e) => taskTitleChange(e)}
+            />
+            <FormInputContainer
+              label="Description"
+              placeholder="Enter task description (optional)"
+              type="text"
+              value={description}
+              onChange={(e) => descriptionChange(e)}
+            />
+            <FormInputContainer
+              id="due-date"
+              label="Due Date*"
+              type="date"
+              value={dueDate}
+              onChange={(e) => dueDateChange(e)}
+            />
+            <FormInputContainer
+              label="Due Time (Optional)"
+              type="time"
+              value={dueTime}
+              onChange={(e) => dueTimeChange(e)}
+            />
+
+            <hr className="form-horizontal-line"></hr>
+
+            <Switch onClick={() => enableEmailAlert()} />
+
+            {isEmailAlertEnabled && (
+              <EmailInputContainer
+                emailAddress={emailAddress}
+                alertDate={alertDate}
+                alertTime={alertTime}
+                emailAddressChange={emailAddressChange}
+                alertDateChange={(e) => alertDateChange(e)}
+                alertTimeChange={(e) => alertTimeChange(e)}
+                addTasks={addTasks}
+              />
+            )}
+          </form>
+          <Button
+            text="Create Task"
+            onClick={() =>
+              addTasks({
+                title: taskTitle,
+                description: description,
+                dueDate: dueDate,
+                dueTime: dueTime,
+                emailAddress: emailAddress,
+                alertDate: alertDate,
+                alertTime: alertTime,
+              })
+            }
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
-if(isEmailAlertEnabled) {
-document.getElementById('form-container-id').style.overflowY = 'scroll'
-document.getElementById('form-container-id').style.scrollbarColor= ''
-document.getElementById('form-container-id').style.scrollbarWidth = 'thin'
-}
-   
-   const [taskTitle, setTaskTitle] = useState('')
-   const taskTitleChange = (e) => {
-   setTaskTitle(e.target.value)
-   }
-   
-   const [description, setDescription] = useState('')
-   const descriptionChange = (e) => {
-   setDescription(e.target.value)
-   }
-   
-   const [dueDate, setDueDate] = useState('')
-   const dueDateChange = (e) => {
-   setDueDate(e.target.value)
-   }
-   
-   const [dueTime, setDueTime] = useState('')
-   const dueTimeChange = (e) => {
-   setDueTime(e.target.value)
-   }
-
-   const [emailAddress, setEmailAddress] = useState('')
-   const emailAddressChange = (e) => {
-   setEmailAddress(e.target.value)
-   }
-
-
-    const [alertDate,setAlertDate] = useState('')
-   const alertDateChange = (e) => {
-   setAlertDate(e.target.value)
-   }
-
-
-   const [alertTime,setAlertTime] = useState('')
-   const alertTimeChange = (e) => {
-   setAlertTime(e.target.value)
-   }
-
-   // if (isEmailAlertEnabled) {
-   // if (emailAddress.trim() === '' || alertDate.trim() === '') {
-   // document.getElementById('email-address').style.outline = '5px solid oklch(70.9% 0.00008 271.152 / 0.722)'
-   // document.getElementById('alert-date').style.outline = '5px solid oklch(70.9% 0.00008 271.152 / 0.722)'
-   // }
-   // }
-    
-
-   
-
-
-
-return (
-<>
-<div className='form-background'>
-<div className="form-container" id='form-container-id'>
-<div className='form-exit-container'>
-<button className='form-exit-btn' onClick={onClick}>
-<img src={crossIcon}></img>
-</button>
-</div>
-<form>
-<div className="form-container-heading">
-<h2>Create New Task</h2>  
-<p>Add a new task with a due date and optional email alerts.</p>
-</div>
-
-<FormInputContainer id='task-title' autofocus= 'autofocus' label='Task Title*' placeholder='Enter task title' type='text' value={taskTitle} onChange={(e) => taskTitleChange(e)}/>
-<FormInputContainer  label='Description' placeholder='Enter task description (optional)' type='text' value={description} onChange={(e) => descriptionChange(e)}/>
-<FormInputContainer id='due-date' label='Due Date*' type='date' value={dueDate} onChange={(e) => dueDateChange(e)}/>
-<FormInputContainer label='Due Time (Optional)' type='time' value={dueTime} onChange={(e) => dueTimeChange(e)}/>
-
-<hr className='form-horizontal-line'></hr>
-
-<Switch onClick={() => enableEmailAlert ()}/>
-
-{isEmailAlertEnabled &&
-<div className='form-email-alert-state-container'>
-<FormInputContainer id='email-address' value={emailAddress} onChange={(e) => emailAddressChange(e)} label='Email Address*' placeholder='your@email.com' type='email'/>
-<FormInputContainer id='alert-date' value={alertDate} onChange={(e) => alertDateChange(e)} label='Alert Date*'  type='date'/>
-<FormInputContainer value={alertTime} onChange={(e) => alertTimeChange(e)} label='Alert Time (optional)'  type='time'/>
-<p>If no time is specified, alert will be set for all-day.</p>
-</div>
+function FormInputContainer({
+  label,
+  placeholder,
+  type,
+  value,
+  onChange,
+  style,
+  autofocus,
+  id,
+}) {
+  return (
+    <div className="form-input-container">
+      <label>
+        {label}
+        <br></br>
+        <input
+          id={id}
+          autoFocus={autofocus}
+          value={value}
+          type={type}
+          placeholder={placeholder}
+          onChange={onChange}
+          style={style}
+        ></input>
+      </label>
+    </div>
+  );
 }
 
-</form>
-<Button text='Create Task' onClick={() => addTasks({title:taskTitle, description:description,dueDate: dueDate,
-dueTime: dueTime, emailAddress:emailAddress, alertDate:alertDate, alertTime:alertTime})} />
-</div>
-</div>
-</>
-);
+function EmailInputContainer({
+  emailAddress,
+  alertTime,
+  alertDate,
+  emailAddressChange,
+  alertTimeChange,
+  alertDateChange,
+  addTasks,
+}) {
+  // useEffect(() => {
+  //   if (emailAddress.trim() === "" || alertDate.trim() === "") {
+  //     document.getElementById("email-address").style.outline =
+  //       "5px solid oklch(70.9% 0.00008 271.152 / 0.722)";
+  //     document.getElementById("alert-date").style.outline =
+  //       "5px solid oklch(70.9% 0.00008 271.152 / 0.722)";
+  //   }
+  // });
+
+  return (
+    <div className="form-email-alert-state-container">
+      <FormInputContainer
+        value={emailAddress}
+        onChange={emailAddressChange}
+        id="email-address"
+        label="Email Address*"
+        placeholder="your@email.com"
+        type="email"
+      />
+      <FormInputContainer
+        value={alertDate}
+        onChange={alertDateChange}
+        id="alert-date"
+        label="Alert Date*"
+        type="date"
+      />
+      <FormInputContainer
+        value={alertTime}
+        onChange={alertTimeChange}
+        label="Alert Time (optional)"
+        type="time"
+      />
+      <p>If no time is specified, alert will be set for all-day.</p>
+    </div>
+  );
 }
-
-
-
-function FormInputContainer ({label, placeholder, type, value, onChange, style, autofocus, id}) {
-
-return (
-<div className="form-input-container">
-<label>
-{label}<br></br>
-<input id={id} autoFocus={autofocus} value={value} type={type} placeholder={placeholder} onChange={onChange} style={style}></input>
-</label>
-</div>
-);
-}
-

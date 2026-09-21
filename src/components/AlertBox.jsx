83038efaf4c0a93d0c49b8a359b0e9be2fbe2a24
l0la960 <oyinlolaolaniyan47@gmail.com>
2026-09-21 17:10:08 +0100

    scroll bar on mobile and tablet

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -4,7 +4,7 @@ export default function Alert ({ icon,ref}) {
 return (
 <div className="alert-box-container" id='alert-box-container-id' ref={ref}>
 <div className="alert-box"  id='alert-box-id' >
-<div className="alert-box-icon-container"><img className='icon'  ></img></div>
+<div className="alert-box-icon-container"><img className='icon' ></img></div>
 <div className="alert-box-message-container"> </div>
 </div>
 </div>
